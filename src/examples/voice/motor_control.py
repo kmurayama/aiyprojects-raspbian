0306bc17a8fa6b08ab96efcfbd0d028bd78b79b7
Kentaro Murayama <km7813a@american.edu>
2018-08-12 03:04:51 -0400

    Add motor control script

---
+++ src/examples/voice/motor_control.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+# Voice Controlled Motor
+# Modified by Kentaro Murayama
+# 2018-08-12
+# 
+
+"""Run a recognizer using the Google Assistant Library.
+
+The Google Assistant Library has direct access to the audio API, so this Python
+code doesn't need to record audio. Hot word detection "OK, Google" is supported.
+
+It is available for Raspberry Pi 2/3 only; Pi Zero is not supported.
+"""
+
+import logging
+import platform
+import subprocess
+import sys
+
+import aiy.assistant.auth_helpers
+from aiy.assistant.library import Assistant
+import aiy.audio
+import aiy.voicehat
+from google.assistant.library.event import EventType
+
+# Set up GPIO
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+motor1 = PWMOutputDevice(4) # Can adjust speed simply
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+)
+
+# Control motor
+def motor_on_full():
+    aiy.audio.say('Motor On. Full Speed')
+    motor1.value = 1
+    GPIO.output(26, False)
+    GPIO.output(6,  False)
+    GPIO.output(13, True)
+
+def motor_on_half():
+    aiy.audio.say('Motor On. Half Speed')
+    motor1.value = 0.5
+    GPIO.output(26, False)
+    GPIO.output(6,  True)
+    GPIO.output(13, False)
+
+def motor_off():
+    aiy.audio.say('Motor Off.')
+    motor1.value = 0
+    GPIO.output(26, True)
+    GPIO.output(6,  False)
+    GPIO.output(13, False)
+
+
+# Ready functions
+def power_off_pi():
+    aiy.audio.say('Good bye!')
+    subprocess.call('sudo shutdown now', shell=True)
+
+
+def reboot_pi():
+    aiy.audio.say('See you in a bit!')
+    subprocess.call('sudo reboot', shell=True)
+
+
+def say_ip():
+    ip_address = subprocess.check_output("hostname -I | cut -d' ' -f1", shell=True)
+    aiy.audio.say('My IP address is %s' % ip_address.decode('utf-8'))
+
+
+# Main function for voice command handling
+def process_event(assistant, event):
+    print(event)
+    status_ui = aiy.voicehat.get_status_ui()
+    if event.type == EventType.ON_START_FINISHED:
+        status_ui.status('ready')
+        if sys.stdout.isatty():
+            print('Say "OK, Google" then speak, or press Ctrl+C to quit...')
+
+    elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
+        status_ui.status('listening')
+
+    elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED and event.args:
+        print('You said:', event.args['text'])
+        text = event.args['text'].lower()
+        if text == 'power off':
+            assistant.stop_conversation()
+            power_off_pi()
+        elif text == 'reboot':
+            assistant.stop_conversation()
+            reboot_pi()
+        elif text == 'ip address':
+            assistant.stop_conversation()
+            say_ip()
+        elif text == 'motor on':
+            assistant.stop_conversation()
+            motor_on_full()
+        elif text == 'motor half':
+            assistant.stop_conversation()
+            motor_on_half()
+        elif text == 'motor off':
+            assistant.stop_conversation()
+            motor_off()
+
+    elif event.type == EventType.ON_END_OF_UTTERANCE:
+        status_ui.status('thinking')
+
+    elif (event.type == EventType.ON_CONVERSATION_TURN_FINISHED
+          or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT
+          or event.type == EventType.ON_NO_RESPONSE):
+        status_ui.status('ready')
+
+    elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
+        sys.exit(1)
+
+
+def main():
+    if platform.machine() == 'armv6l':
+        print('Cannot run hotword demo on Pi Zero!')
+        exit(-1)
+
+    credentials = aiy.assistant.auth_helpers.get_assistant_credentials()
+    with Assistant(credentials) as assistant:
+        for event in assistant.start():
+            process_event(assistant, event)
+
+
+if __name__ == '__main__':
+    main()
