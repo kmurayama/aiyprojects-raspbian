e5d8a74c18432dae14b7e8a023c6bc896411c382
Kentaro Murayama <km7813a@american.edu>
2018-08-18 16:51:06 +0000

    Test strobe

--- src/examples/voice/gpiozero/strobe.py
+++ src/examples/voice/gpiozero/strobe.py
@@ -1,13 +1,14 @@
 from gpiozero import LED
 import time
 from time import sleep
+from signal import pause
 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 from gpiozero import PWMOutputDevice
 
-strobe = PWMLED(26)
+strobe = LED(26)
 motor = PWMOutputDevice(4)
 
 strobe.off()
@@ -16,7 +17,11 @@ t_end = time.time() + 10
 freq = 15
 step = 1/freq
 
-while time.time() < t_end:
-    motor1.value = 0.5
-    strobe.blink(on_time = step, off_time = step)
+strobe.blink(on_time = step, off_time = step)
+motor.value = 0.3
+pause()
+
+#while time.time() < t_end:
+#    motor1.value = 0.5
+#    strobe.blink(on_time = step, off_time = step)
 
