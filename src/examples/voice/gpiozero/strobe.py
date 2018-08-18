dfb6908a62e56a87df124c20e6b3794b0e48e138
Kentaro Murayama <km7813a@american.edu>
2018-08-18 00:05:35 -0400

    Add PWM control for LED and motor

--- src/examples/voice/gpiozero/strobe.py
+++ src/examples/voice/gpiozero/strobe.py
@@ -2,7 +2,13 @@ from gpiozero import LED
 import time
 from time import sleep
 
-strobe = LED(26)
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+from gpiozero import PWMOutputDevice
+
+strobe = PWMLED(26)
+motor = PWMOutputDevice(4)
 
 strobe.off()
 
@@ -11,8 +17,6 @@ freq = 15
 step = 1/freq
 
 while time.time() < t_end:
-    sleep(step)
-    strobe.on()
-    sleep(step)
-    strobe.off()
+    motor1.value = 0.5
+    strobe.blink(on_time = step, off_time = step)
 
